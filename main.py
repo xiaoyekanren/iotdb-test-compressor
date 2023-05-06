a6f039c989cc86e04dfcf0f2f96f88d54de5496c
xiaoyekanren <876670773@qq.com>
2023-05-06 15:11:51 +0800

    优化流程，增加新列query_time,确定查询开始的时间

--- main.py
+++ main.py
@@ -38,12 +38,12 @@ def get_csv(column, row, datatype):
     exit()
 
 
-def generate_test_timeseries_list_by_column(sql, columns):
+def generate_test_timeseries_list_by_column(sql, timeseries, columns):
     ts_list = []
-    timeseries, datatype, encoding, compressor = common.split_sql(sql)
-    for column in range(columns):
-        next_level_ts = (sql.replace(timeseries, timeseries + '.' + 'column_' + str(column))).rstrip(';')  # 是因为创建时间序列的时候，会将list转为使用分号的分隔的string
-        ts_list.append(next_level_ts)
+    for column in range(columns):  # 确定本次测试有多少列，就生成多少个创建时间序列的sql
+        ts = f'{timeseries}.column_{column}'
+        next_level_ts = sql.replace(timeseries, ts)
+        ts_list.append(next_level_ts.rstrip(';'))  # 无需保留最后的分号，iotdb_operation在创建时间序列的时候，会将list转为使用分号的分隔的string，
     return ts_list
 
 
@@ -148,9 +148,10 @@ def iotdb_operation(csv_file, test_create_sql_list):
     # flush
     iotdb_exec_by_cli('flush', info='flush')
     # ！！！查询！！！
+    query_time = int(time.time() * 1000)  # 启动查询的时间，默认是秒，转毫秒好看
     query_elapsed_time = iotdb_query(test_create_sql_list, info='开始查询')
 
-    return import_elapsed_time, query_elapsed_time
+    return import_elapsed_time, query_time, query_elapsed_time
 
 
 def check_result_file(datatype, encoding, compressor, column, row):
@@ -171,79 +172,72 @@ def write_to_result_file(result):
         result_log.writelines('\n' + result)
 
 
-def try_re_test(csv_file, test_create_sql_list):
+def retry_test(csv_file, test_create_sql_list):
     iotdb_stop()
     time.sleep(10)
     iotdb_rm_data()
     time.sleep(3)
     iotdb_start()
     time.sleep(10)
-    import_elapsed_time, query_elapsed_time = iotdb_operation(csv_file, test_create_sql_list)
-    return import_elapsed_time, query_elapsed_time
+    return iotdb_operation(csv_file, test_create_sql_list)
 
 
 def main():
-    has_result_file = None
     # 判断是否存在result文件
+    has_result_file = None
     if os.path.isfile(output_result_log_file):
+        print('info: 检测到已有结果文件.')
         has_result_file = True
-
-    # 尝试停止现有iotdb，并清理数据
-    # iotdb_stop()
-    # iotdb_rm_data()
 
     # 生成全部的sql文件列表
     create_timeseries_list = common.generate_all_timeseries()
 
-    # 输出title
-    print('result', 'datatype', 'encoding', 'compressor', 'column', 'row', 'start_time/ms', 'end_time/ms', 'import_elapsed_time/s', 'query_elapsed_time/s', 'data_size/b', 'compression_rate', 'tsfile_count', sep=',')
+    # 输出csv的title
+    print('result', 'datatype', 'encoding', 'compressor', 'column', 'row', 'start_time/ms', 'query_time/ms', 'end_time/ms', 'import_elapsed_time/s', 'query_elapsed_time/s', 'data_size/b', 'compression_rate', 'tsfile_count', sep=',')
 
-    # 遍历，主程序
+    # 主程序，遍历create_timeseries_list
     for create_sql in create_timeseries_list:
-        has_result = None
-        # 将sql拆分
-        timeseries, datatype, encoding, compressor = common.split_sql(create_sql)
-
-        # 序列的多种用例的遍历
-        # 列循环，config.ini中的csv_column
+        # 列遍历，config.ini中的csv_column
         for column in csv_column:
-            # 行循环， config.ini中的csv_row
+            # 行遍历， config.ini中的csv_row
             for row in csv_row:
+                # 从sql里面拆出来各项
+                timeseries, datatype, encoding, compressor = common.split_sql(create_sql)
+
                 # 检测进度
                 print(f'info: 开始执行 {datatype}-{encoding}-{compressor}-{column}-{row}, 当前第{create_timeseries_list.index(create_sql)}个，剩余{len(create_timeseries_list) - create_timeseries_list.index(create_sql)}个')
 
                 # 检查是否有历史记录：
+                has_result = None  # 声明has_result，不然后面会报警
                 if has_result_file:
                     has_result = check_result_file(datatype, encoding, compressor, column, row)
                 if has_result:
                     print(f'info: 检测到 {datatype},{encoding},{compressor},{column},{row} 已经测试完毕，跳过.')
                     continue
 
-                # 启动iotdb
-                iotdb_start()
-                # 启动时间
-                start_time = int(time.time() * 1000)
+                # 基于当前create_sql，在path后追加一段，用于区分多列（1，3，5列）
+                # root.g1.boolean.plain.uncompressed.column_<0/1/2/3/4>，column_0，column_1，column_2...
+                test_create_sql_list = generate_test_timeseries_list_by_column(create_sql, timeseries, int(column))
 
-                # 再重新拆分create-sql
+                # 确定csv路径，csv大小
                 csv_file, csv_size = get_csv(column, row, datatype.lower())
-
-                # 生成下一级时间序列的list，root.g1.boolean.plain.uncompressed.column_<0/1/2/3/4>
-                test_create_sql_list = generate_test_timeseries_list_by_column(create_sql, int(column))
-
                 # 替换csv文件的title
                 replace_csv_title(test_create_sql_list, csv_file)
 
-                # 主程序
-                import_elapsed_time, query_elapsed_time = iotdb_operation(csv_file, test_create_sql_list)
+                # 启动iotdb
+                iotdb_start()  # 就当iotdb是全新的，没有启动
+                start_time = int(time.time() * 1000)  # 启动时间，默认是秒，转毫秒好看
+
+                # 主程序·主程序
+                import_elapsed_time, query_time, query_elapsed_time = iotdb_operation(csv_file, test_create_sql_list)
 
                 # 统计数据文件大小
                 data_size, tsfile_count = iotdb_get_data_size()
-
-                # 通过判断的大小来确定是否执行成功，如果失败，增加sleep时间，默认重试5次
+                # 通过判断tsfile的大小和数量来确定是否执行成功，如果失败，增加sleep时间，默认重试5次
                 for i in range(retry_times):
                     if data_size == 0 or tsfile_count == 0:
                         print('info: 检测到问题，重新本次测试.')
-                        import_elapsed_time, query_elapsed_time = try_re_test(csv_file, test_create_sql_list)
+                        import_elapsed_time, query_time, query_elapsed_time = retry_test(csv_file, test_create_sql_list)
                         data_size, tsfile_count = iotdb_get_data_size()
                         continue
                     else:
@@ -254,11 +248,12 @@ def main():
 
                 # 统计压缩率
                 compression_rate = round(csv_size / data_size, 5)
+
                 # 结束时间
                 end_time = int(time.time() * 1000)
 
                 # 打印、存储结果
-                result = f'result,{datatype},{encoding},{compressor},{column},{row},{start_time},{end_time},{import_elapsed_time},{query_elapsed_time},{data_size},{compression_rate},{tsfile_count}'
+                result = f'result,{datatype},{encoding},{compressor},{column},{row},{start_time},{query_time},{end_time},{import_elapsed_time},{query_elapsed_time},{data_size},{compression_rate},{tsfile_count}'
                 print(result)
                 write_to_result_file(result)
 
